7ab5fe18dc9786bae97cb94f447effa530f27da8
NikitaVisheratin <visheratinn@gmail.com>
2021-10-06 21:04:14 +0300

    first file

---
+++ eb.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+int main()
+{
+    int i, N, p;
+    std::cin >> N;
+    for (i = 1; i <= N; i++) 
+    {
+        std::cin >> p;
+        std::cout << (p*(p-1)) / 2;
+    }
+    return 0;
+}
